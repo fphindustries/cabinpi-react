351ee15bdc368b0cbb81caf55fb8c9edeb0f3a99
Christopher Kent <cpkent@gmail.com>
2025-11-19 18:16:52 -0800

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.55.0
      project name = cabinpi-react
      framework = react
      framework cli = create-vite@7.1.1
      package manager = npm@11.6.3
      wrangler = wrangler@4.49.0
      git = 2.48.1.windows.1

--- src/app/api/solar/route.js
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic' // defaults to auto
-import { fetchSolarHistory } from "@/app/lib/data"
-
-export async function GET(request) {
-    let data = await fetchSolarHistory();
-    return Response.json(data);
-
-}
